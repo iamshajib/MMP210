8908bd04fe0e7f832efe833ed5e6ed4ee8127627
Mohammed Shajib <mohammed.shajib@f1006-01-34125.bmcc.cuny.edu>
2015-10-19 17:13:58 -0400

    class ses

--- WEEK-6/inClass/js/main.js
+++ WEEK-6/inClass/js/main.js
@@ -1,11 +1,47 @@
 function setup() {
-    var myCanvas = createCanvas(640, 480);
-    myCanvas.parent('myContainer');
-}
+
+    createCanvas(windowWidth, windowHeight);
+
+
+    //    drawWindow(50,50); //call the function
+    //    drawWindow(200, 50); //call the function
+    //    drawWindow(350, 50); //call the function
+
+    var x = 0;
+    var y = 0;
+    var gridWidth = width - 100;
+
+    for (var i = 0; i < 8; i++) {
+        console.log(i);
+        drawWindow(x, y);
+        //increment our X
+        x += 150; //same as x=x+120
+
+        //check i fx is past the grid width
+        if (x > gridWidth) {
+            // set x back to the begining
+            x = 0;
+            //move y down
+            y += 270;
+        } //end if
+    } //end for loop
+} //end setup
 
 
 
-function draw() {
-    rect (50, 50, 50, 50);
+
+
+function drawWindow(startX, startY) {
+
+    fill('green');
+    var winWidth = 100;
+    var winHeight = 250;
+    rect(startX, startY, winWidth, winHeight);
+    //window lines
+    line(startX + winWidth/2, startY, startX + winWidth/2, startY + winHeight);
+
+    line(startX, startY *.333, startX + winWidth, startY *.333);
+    line(startX, startY *.666, startX + winWidth, startY *.666);
+
 
 }
