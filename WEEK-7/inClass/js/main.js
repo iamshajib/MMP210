e25910bf2d1fd537715862ae6b980e8e6b206393
Mohammed Shajib <mohammed.shajib@F1006-01-34125.bmcc.cuny.edu>
2015-10-26 15:46:55 -0400

    bubble with objects

--- WEEK-7/inClass/js/main.js
+++ WEEK-7/inClass/js/main.js
@@ -1,3 +1,15 @@
+//bubble Consyructor
+function Bubble(){
+    //create x and y properties (variables)
+    this.x = random(width);
+    this.y = random(height);
+    this.r = 20; //r is for radious wich is one half the width of the circle
+    this.bubbleSize = this.r*2; //width and height are twich the radius
+    //draw the bubble
+    ellipse(this.x, this.y, this.bubbleSize, this.bubbleSize);
+}//end bubble
+
+
 function setup(){
     //create a canvas. put into a variavle to associate with an element
     var cnv = createCanvas(800,3000);
@@ -6,23 +18,27 @@ function setup(){
     var myArray = [];//create blank array
     //add stuff to the array with a loop
 
-    for(var i=0; i<100; i++){
-        myArray[i]="item"+i;
 
-        myArray[i] = ellipse(i,i,10,10);
+
+    for(var i=0; i<10; i++){
+        //add bubbles to array
+        myArray[i] = new Bubble();
     }
+//logout the array
+    console.log(myArray);
+
+
 
     //loop to go through every item of the array
 
     for(var i=0; i<myArray.length; i++){
 
-        myArray[i]+="Shajib";
+//        myArray[i]+="Shajib";
+
         console.log(myArray[i]);
     }
 
-//    console.log(myArray[0]);
-//    console.log(myArray[1]);
-//    console.log(myArray[2]);
-//    console.log(myArray);
-//    console.log("myArray", myArray);
+     console.log(myArray[i]);
+
+
 }//end setup
---
+++ WEEK-7/inClass/js/thing1.js
@@ -0,0 +1,37 @@
+
+
+function setup(){
+    //create a canvas. put into a variavle to associate with an element
+    var cnv = createCanvas(800,3000);
+    cnv.parent("window");
+
+    var myArray = [];//create blank array
+    //add stuff to the array with a loop
+
+
+
+    for(var i=0; i<100; i++){
+        myArray[i]="item"+i;
+
+
+    }
+
+
+    for(var i=0; i<myArray.length; i++){
+
+        myArray[i]+="Shajib";
+
+        console.log(myArray[i]);
+    }
+
+
+
+
+
+
+//    console.log(myArray[0]);
+//    console.log(myArray[1]);
+//    console.log(myArray[2]);
+//    console.log(myArray);
+//    console.log("myArray", myArray);
+}//end setup
