aca9227eb43b19bca2064978bc27810850788daa
Mohammed Shajib <mohammed.shajib@F1006-01-34125.bmcc.cuny.edu>
2015-11-02 16:28:32 -0500

    modify alien

--- MID/js/main.js
+++ MID/js/main.js
@@ -1,10 +1,11 @@
 var leftEye_x = 78;
 var rightEye_x = 135;
-var mouth = 105;
+var mouth_y = 70;
+var line_x = 120;
 
 function setup(){
 	var myCanvas = createCanvas(window.innerWidth, window.innerHeight);
-	myCanvas.parent("myPSContainer");
+
 }
 
 
@@ -13,18 +14,27 @@ function draw() {
 	clear();
 
 	if (mouseIsPressed) {
-//		noStroke();
-//		fill(244, 35, 0);
-//		leftEye_x = leftEye_x +1;
-//		rightEye_x = rightEye_x + 1;
-        fill("red");
+		stroke(0);
+		fill(244, 35, 0);
+		leftEye_x = leftEye_x -1;
+		rightEye_x = rightEye_x + 1;
+        line_x = line_x + 1;
+
+        mouth_y = mouth_y + 1;
+
 		ellipse(leftEye_x, 70, 16, 32);
 		ellipse(rightEye_x, 70, 16, 32);
-        ellipse(mouth, 100, 32, 10);
-        ellipse(110, 165, 50, 130);
+        ellipse(110, mouth_y, 60, 60);
+
+        line(90,line_x, 50, line_x+60);
+
 
 
 	} else {
+         leftEye_x = 78;
+ rightEye_x = 135;
+ mouth_y = 70;
+ line_x = 120
 		stroke(0);
 		fill("rgba(51,51,51,0.5)");
 		// rect(100, 100, 20, 100);
